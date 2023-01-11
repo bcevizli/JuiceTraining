e92cd2dc635dcae92b7e830b12da50f18c3a07a7
burak cevizli <burak.cevizli7@gmail.com>
2023-01-11 17:04:09 +0300

    Initial Commit

---
+++ juicetraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  juicetraining
+//
+//  Created by Adem Burak Cevizli on 11.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
